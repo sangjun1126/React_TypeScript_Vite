f14a049902c102264b2c1ac76266346dc59d55f6
sangjun1126 <jeongsangjun1234@gmail.com>
2024-01-30 14:40:23 +0900

    commit 0130

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import Profile from './MyButton';
-import './index.css'
+import { useState } from "react";
+import TicTacToe from "./TicTacToe";
 
-function App() {
-    return <Profile />;
+export default function App() {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+
+    return (
+        <div>
+            <h1>카운터 만들어보기</h1>
+            <MyButton count = {count} onClick = {handleClick} />
+            <MyButton count = {count} onClick = {handleClick} />
+        </div>
+    )
 }
 
-export default App;
+function MyButton({count, onClick}) {
+    return (
+        <button onClick={onClick}>
+            Click {count} times
+        </button>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import TicTacToe from './TicTacToe.tsx'
+import ProjectCate from './ProjectCategoryRow.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ProjectCate />
   </React.StrictMode>,
 )
 
